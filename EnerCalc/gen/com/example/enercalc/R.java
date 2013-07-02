582f9c8204e371db97e9f9b4e0a70827d2311f95
Feranick <feranick@hotmail.com>
2013-07-02 14:39:43 -0400

    Rewritten with the introduction of increments. New layout Bug fixes

--- EnerCalc/gen/com/example/enercalc/R.java
+++ EnerCalc/gen/com/example/enercalc/R.java
@@ -14,22 +14,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int convBtn=0x7f070009;
+        public static final int convBtn=0x7f07000e;
+        public static final int decBtn=0x7f070011;
         public static final int editJ=0x7f070003;
         public static final int editK=0x7f070004;
         public static final int editeV=0x7f070000;
         public static final int edithz=0x7f070005;
+        public static final int editinc=0x7f070012;
         public static final int editnm=0x7f070002;
         public static final int editry=0x7f070006;
         public static final int editwn=0x7f070001;
-        public static final int resetBtn=0x7f070007;
-        public static final int viewJ=0x7f07000f;
-        public static final int viewK=0x7f070008;
-        public static final int vieweV=0x7f07000e;
-        public static final int viewhz=0x7f07000c;
-        public static final int viewnm=0x7f07000b;
-        public static final int viewry=0x7f07000a;
-        public static final int viewwn=0x7f07000d;
+        public static final int incBtn=0x7f070010;
+        public static final int resetBtn=0x7f07000f;
+        public static final int viewJ=0x7f07000d;
+        public static final int viewK=0x7f07000c;
+        public static final int vieweV=0x7f07000b;
+        public static final int viewhz=0x7f070009;
+        public static final int viewnm=0x7f070008;
+        public static final int viewry=0x7f070007;
+        public static final int viewwn=0x7f07000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,14 +43,17 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int convBtn=0x7f04000d;
+        public static final int decBtn=0x7f040014;
         public static final int editJ=0x7f040003;
         public static final int editK=0x7f040007;
         public static final int editeV=0x7f040005;
         public static final int edithz=0x7f04000f;
+        public static final int editinc=0x7f040015;
         public static final int editnm=0x7f04000b;
         public static final int editry=0x7f040011;
         public static final int editwn=0x7f040009;
         public static final int hello_world=0x7f040001;
+        public static final int incBtn=0x7f040013;
         public static final int menu_settings=0x7f040002;
         public static final int resetBtn=0x7f04000e;
         public static final int viewJ=0x7f040004;
--- EnerCalc/src/com/example/enercalc/MainActivity.java
+++ EnerCalc/src/com/example/enercalc/MainActivity.java
@@ -26,12 +26,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		EditText editJ = (EditText) findViewById(R.id.editJ);
-		editJ.requestFocus();
-		editJ.setText("1.602E-19");
+		EditText editeV = (EditText) findViewById(R.id.editeV);
+		EditText editinc = (EditText) findViewById(R.id.editinc);
+		editeV.requestFocus();
+		editeV.setText("1.0");
+		editinc.setText("0.1");
 		
 		MainActivity testing = this;
-		testing.convert(editJ);
+		testing.convert(editeV);
 	}
 
 	@Override
@@ -61,7 +63,7 @@ public class MainActivity extends Activity {
 		String nm = "";
 		String mhz = "";
 		String ry = "";
-	
+
 		
 		if(editJ.hasFocus() && editJ.getText().toString().compareTo("")!=0)
 			{double Jd = Double.valueOf(editJ.getText().toString());
@@ -166,37 +168,111 @@ public class MainActivity extends Activity {
 	
 	
 	public void reset(View view) {
-		EditText editJ = (EditText) findViewById(R.id.editJ);		
-		editJ.setText("1.602E-19");
-	    editJ.requestFocus();
-	    convert(editJ);
+		EditText editeV = (EditText) findViewById(R.id.editeV);		
+	    EditText editinc = (EditText) findViewById(R.id.editinc);	
+	    editeV.setText("1.0");
+	    editeV.requestFocus();
+	    editinc.setText("0.1");
+	    convert(editeV);
 		
 	}
 	
 	public void clearfield(View view) {
-		EditText editJ = (EditText) findViewById(R.id.editJ);
 		EditText editeV = (EditText) findViewById(R.id.editeV);
-		EditText editK = (EditText) findViewById(R.id.editK);
 		EditText editwn = (EditText) findViewById(R.id.editwn);
 		EditText editnm = (EditText) findViewById(R.id.editnm);
+		EditText editJ = (EditText) findViewById(R.id.editJ);
+		EditText editK = (EditText) findViewById(R.id.editK);
+		EditText edithz = (EditText) findViewById(R.id.edithz);
+		EditText editry = (EditText) findViewById(R.id.editry);
+		EditText editinc = (EditText) findViewById(R.id.editinc);
+		
+		if(editeV.hasFocus())
+			{editeV.setText("");
+			editinc.setText("0.1");}
+		if(editwn.hasFocus())
+			{editwn.setText("");
+			editinc.setText("100");}
+		if(editnm.hasFocus())
+			{editnm.setText("");
+			editinc.setText("50");}
+		if(editJ.hasFocus())
+			{editJ.setText("");
+			editinc.setText("0.1e-19");}
+		if(editK.hasFocus())
+			{editK.setText("");
+			editinc.setText("100");}
+		if(edithz.hasFocus())
+			{edithz.setText("");
+			editinc.setText("100");}
+		if(editry.hasFocus())
+			{editry.setText("");
+			editinc.setText("0.01");}
+	}
+	
+	public void change(View view, double incr) {
+		//EditText editinc = (EditText) findViewById(R.id.editinc);
+		EditText editeV = (EditText) findViewById(R.id.editeV);
+		EditText editwn = (EditText) findViewById(R.id.editwn);
+		EditText editnm = (EditText) findViewById(R.id.editnm);
+		EditText editJ = (EditText) findViewById(R.id.editJ);
+		EditText editK = (EditText) findViewById(R.id.editK);
 		EditText edithz = (EditText) findViewById(R.id.edithz);
 		EditText editry = (EditText) findViewById(R.id.editry);
 		
-		if(editJ.hasFocus())
-			editJ.setText("");
-		if(editeV.hasFocus())
-			editeV.setText("");
-		if(editK.hasFocus())
-			editK.setText("");
-		if(editwn.hasFocus())
-			editwn.setText("");
-		if(editnm.hasFocus())
-			editnm.setText("");
-		if(edithz.hasFocus())
-			edithz.setText("");
-		if(editry.hasFocus())
-			editry.setText("");
+		//double incr = Double.valueOf(editinc.getText().toString());
+		
+		NumberFormat nF  = new DecimalFormat("0.000E0");
+		DecimalFormat dec2 = new DecimalFormat("0.00");
+		DecimalFormat dec3 = new DecimalFormat("0.000");
+		
+		if(editeV.hasFocus() && editeV.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(editeV.getText().toString()) + incr;
+			editeV.setText(dec2.format(d));
+			convert(editeV);}
+		
+		if(editwn.hasFocus() && editwn.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(editwn.getText().toString()) + incr;
+			editwn.setText(dec2.format(d));
+			convert(editwn);}
+		
+		if(editnm.hasFocus() && editnm.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(editnm.getText().toString()) + incr;
+			editnm.setText(dec2.format(d));
+			convert(editnm);}
+		
+		if(editJ.hasFocus() && editJ.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(editJ.getText().toString()) + incr;
+			editJ.setText(nF.format(d));
+			convert(editJ);}
+		
+		if(editK.hasFocus() && editK.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(editK.getText().toString()) + incr;
+			editK.setText(dec2.format(d));
+			convert(editK);}
+		
+		if(edithz.hasFocus() && edithz.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(edithz.getText().toString()) + incr;
+			edithz.setText(nF.format(d));
+			convert(edithz);}
+		
+		if(editry.hasFocus() && editry.getText().toString().compareTo("")!=0)
+			{double d = Double.valueOf(editry.getText().toString()) + incr;
+			editry.setText(dec3.format(d));
+			convert(editry);}
 		
 	}
+	
+	public void inc(View view)
+		{EditText editinc = (EditText) findViewById(R.id.editinc);
+		double incr = Double.valueOf(editinc.getText().toString());
+		change(view, incr);	
+		}
+	
+	public void dec(View view)
+		{EditText editinc = (EditText) findViewById(R.id.editinc);
+		double incr = Double.valueOf(editinc.getText().toString());
+		change(view, -1.00*incr);	
+		}
 	
 }
