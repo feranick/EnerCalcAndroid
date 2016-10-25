896465639455fdcc0ee3c2d44f57ac2901fb5fbf
Nicola Ferralis <feranick@hotmail.com>
2016-10-25 15:57:12 -0400

    Version 2.0-20161025
    Also added new icons.

--- EnerCalc/build/generated/source/r/debug/feranick/enercalc/R.java
+++ EnerCalc/build/generated/source/r/debug/feranick/enercalc/R.java
@@ -14,56 +14,59 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int convBtn=0x7f08000e;
-        public static final int decBtn=0x7f080011;
-        public static final int editJ=0x7f080003;
-        public static final int editK=0x7f080004;
-        public static final int editeV=0x7f080000;
-        public static final int edithz=0x7f080005;
-        public static final int editinc=0x7f08000f;
-        public static final int editnm=0x7f080002;
-        public static final int editry=0x7f080006;
-        public static final int editwn=0x7f080001;
-        public static final int incBtn=0x7f080012;
-        public static final int menuabout=0x7f080013;
-        public static final int resetBtn=0x7f080010;
-        public static final int viewJ=0x7f08000d;
-        public static final int viewK=0x7f08000c;
-        public static final int vieweV=0x7f08000b;
-        public static final int viewhz=0x7f080009;
-        public static final int viewnm=0x7f080008;
-        public static final int viewry=0x7f080007;
-        public static final int viewwn=0x7f08000a;
+        public static final int convBtn=0x7f09000e;
+        public static final int decBtn=0x7f090011;
+        public static final int editJ=0x7f090003;
+        public static final int editK=0x7f090004;
+        public static final int editeV=0x7f090000;
+        public static final int edithz=0x7f090005;
+        public static final int editinc=0x7f09000f;
+        public static final int editnm=0x7f090002;
+        public static final int editry=0x7f090006;
+        public static final int editwn=0x7f090001;
+        public static final int incBtn=0x7f090012;
+        public static final int menuabout=0x7f090013;
+        public static final int resetBtn=0x7f090010;
+        public static final int viewJ=0x7f09000d;
+        public static final int viewK=0x7f09000c;
+        public static final int vieweV=0x7f09000b;
+        public static final int viewhz=0x7f090009;
+        public static final int viewnm=0x7f090008;
+        public static final int viewry=0x7f090007;
+        public static final int viewwn=0x7f09000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_main=0x7f040000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f080000;
+    }
+    public static final class mipmap {
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int convBtn=0x7f050001;
-        public static final int decBtn=0x7f050002;
-        public static final int editJ=0x7f050003;
-        public static final int editK=0x7f050004;
-        public static final int editeV=0x7f050005;
-        public static final int edithz=0x7f050006;
-        public static final int editinc=0x7f050007;
-        public static final int editnm=0x7f050008;
-        public static final int editry=0x7f050009;
-        public static final int editwn=0x7f05000a;
-        public static final int incBtn=0x7f05000b;
-        public static final int menu_settings=0x7f05000c;
-        public static final int menuabout=0x7f05000d;
-        public static final int resetBtn=0x7f05000e;
-        public static final int viewJ=0x7f05000f;
-        public static final int viewK=0x7f050010;
-        public static final int vieweV=0x7f050011;
-        public static final int viewhz=0x7f050012;
-        public static final int viewnm=0x7f050013;
-        public static final int viewry=0x7f050014;
-        public static final int viewwn=0x7f050015;
+        public static final int app_name=0x7f060000;
+        public static final int convBtn=0x7f060001;
+        public static final int decBtn=0x7f060002;
+        public static final int editJ=0x7f060003;
+        public static final int editK=0x7f060004;
+        public static final int editeV=0x7f060005;
+        public static final int edithz=0x7f060006;
+        public static final int editinc=0x7f060007;
+        public static final int editnm=0x7f060008;
+        public static final int editry=0x7f060009;
+        public static final int editwn=0x7f06000a;
+        public static final int incBtn=0x7f06000b;
+        public static final int menu_settings=0x7f06000c;
+        public static final int menuabout=0x7f06000d;
+        public static final int resetBtn=0x7f06000e;
+        public static final int viewJ=0x7f06000f;
+        public static final int viewK=0x7f060010;
+        public static final int vieweV=0x7f060011;
+        public static final int viewhz=0x7f060012;
+        public static final int viewnm=0x7f060013;
+        public static final int viewry=0x7f060014;
+        public static final int viewwn=0x7f060015;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
@@ -74,12 +77,12 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class xml {
-        public static final int searchable=0x7f040000;
+        public static final int searchable=0x7f050000;
     }
 }
