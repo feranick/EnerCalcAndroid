3be94ec6d8695e2f5b7b8b9bae095608496196d3
Nicola Ferralis <feranick@hotmail.com>
2017-12-19 23:52:24 -0500

    Version 2.1-20171219 - Added support for Android Oreo

--- EnerCalc/build/generated/source/buildConfig/debug/feranick/enercalc/BuildConfig.java
+++ EnerCalc/build/generated/source/buildConfig/debug/feranick/enercalc/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "feranick.enercalc";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 31;
-  public static final String VERSION_NAME = "2.0-20161025";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "2.1-20171219";
 }
